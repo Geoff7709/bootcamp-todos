9ca7f4bfdcc2df3d412e07dc0d8aa08b6e9d7d3a
Geoffrey Zimmerman <geoffzimmerman23@gmail.com>
2020-11-21 13:37:44 -0600

    added todo

--- script.js
+++ script.js
@@ -4,6 +4,8 @@ var todoList = document.querySelector("#todo-list");
 var todoCountSpan = document.querySelector("#todo-count");
 
 var todos = ["Learn HTML", "Learn CSS", "Learn JavaScript"];
+
+todoList.innerHTML = ""
 function renderTodos () {
 for (var i = 0; i < todos.length; i++) {
     var li = document.createElement('li');
@@ -11,4 +13,16 @@ for (var i = 0; i < todos.length; i++) {
     todoList.appendChild(li);}
 
 }
+
+function addTodo (event) {
+event.preventDefault(event);
+if (newTodo !== '') 
+{var newTodo = todoInput.value;
+todos.push(newTodo)
+todoInput = null;
+renderTodos ()}
+
+}
+
+todoForm.addEventListener('submit', todoForm)
 renderTodos()
