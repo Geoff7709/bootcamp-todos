4cf4aefd7c51e332fb9ddf4c15ec8aa7f1b4297d
Geoffrey Zimmerman <geoffzimmerman23@gmail.com>
2020-11-24 18:13:22 -0600

    final code

--- script.js
+++ script.js
@@ -3,26 +3,55 @@ var todoForm = document.querySelector("#todo-form");
 var todoList = document.querySelector("#todo-list");
 var todoCountSpan = document.querySelector("#todo-count");
 
-var todos = ["Learn HTML", "Learn CSS", "Learn JavaScript"];
+var todos = JSON.parse(localStorage.getItem('todos')) || []
 
-todoList.innerHTML = ""
-function renderTodos () {
-for (var i = 0; i < todos.length; i++) {
-    var li = document.createElement('li');
+function renderTodos() {
+  // clear existing todos
+  todoList.innerHTML = ''
+  // re-render todos
+  for (var i = 0; i < todos.length; i++) {
+    // create li element
+    var li = document.createElement('li')
+    // add todo text to li
     li.innerText = todos[i]
-    todoList.appendChild(li);}
-
+    // add data-index attribute
+    li.setAttribute('data-index', i)
+    // create button
+    var button = document.createElement('button')
+    button.innerText = "Complete"
+    li.appendChild(button)
+    // append li to todoList
+    todoList.appendChild(li)
+  }
 }
 
-function addTodo (event) {
-event.preventDefault(event);
-if (newTodo !== '') 
-{var newTodo = todoInput.value;
-todos.push(newTodo)
-todoInput = null;
-renderTodos ()}
-
+function addTodo(event) {
+  event.preventDefault()
+  // get todoInput value
+  var newTodo = todoInput.value
+  if (newTodo !== '') {
+    // push value into todos
+    todos.push(newTodo)
+    todoInput.value = null
+    localStorage.setItem('todos', JSON.stringify(todos))
+    renderTodos()
+  }  
 }
 
-todoForm.addEventListener('submit', todoForm)
+function removeTodo(event) {
+  var target = event.target
+  if (target.matches('button')) {
+    // find index from LI
+    var index = parseInt(target.parentNode.getAttribute('data-index'))
+    // remove the todo from todos array
+    todos.splice(index, 1)
+    // re-render todos
+    renderTodos() 
+    localStorage.setItem('todos', JSON.stringify(todos))
+  }
+}
+
+todoForm.addEventListener('submit', addTodo)
+todoList.addEventListener('click', removeTodo)
+
 renderTodos()
